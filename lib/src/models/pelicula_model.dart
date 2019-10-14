68e1c0f4e3625577a09d5ba94cc58524e17e8e87
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 15:18:51 -0500

    Se hizo una nueva clase Peliculas y constructores en el modelo pelicula_model.dart, tambien se hizo un formateo del json para que sea utilizado en un map

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -1,3 +1,18 @@
+class Peliculas {
+    List<Pelicula> items = new List();
+
+    Peliculas();
+
+    Peliculas.fromJsonList(List<dynamic> jsonList) {
+        if(jsonList == null) return;
+
+        for(var item in jsonList){
+            final pelicula = new Pelicula.fromJsonMap(item);
+            items.add(pelicula);
+        }
+    }
+}
+
 class Pelicula {
     double popularity;
     int voteCount;
@@ -15,8 +30,8 @@ class Pelicula {
     String releaseDate;
 
     Pelicula({
-        this.popularity,
         this.voteCount,
+        this.popularity,
         this.video,
         this.posterPath,
         this.id,
@@ -30,4 +45,21 @@ class Pelicula {
         this.overview,
         this.releaseDate,
     });
+
+    Pelicula.fromJsonMap(Map<String, dynamic> json) {
+        voteCount        = json['vote_count'];
+        popularity       = json['popularity'] / 1;
+        video            = json['video'];
+        posterPath       = json['poster_path'];
+        id               = json['id'];
+        adult            = json['adult'];
+        backdropPath     = json['backdrop_path'];
+        originalLanguage = json['original_language'];
+        originalTitle    = json['original_title'];
+        genreIds         = json['genre_ids'].cast<int>();
+        title            = json['title'];
+        voteAverage      = json['vote_average'] / 1;
+        overview         = json['overview'];
+        releaseDate      = json['release_date'];
+    }
 }
