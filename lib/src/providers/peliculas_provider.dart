87e0d94e2b15edb4318434f7ba2595cca6010c61
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 15:59:21 -0500

    se creo una variable resp que almacena la respuesta con un await de http.get y una variable decodeData que almacena el body de la respuesta resp y luego se imprime para prueba

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,3 +1,8 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+//My imports
 import 'package:moviesapp_flutter/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
@@ -10,5 +15,10 @@ class PeliculasProvider {
             'api_key'   : _apikey,
             'language'  : _language
         });
+
+        final resp = await http.get(url);
+        final decodeData = json.decode(resp.body);
+
+        print(decodeData);
     }
 }
