fccc7b2a132b8b7318aaf5d723b1b88268b669c4
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 12:57:53 -0500

    Se crea un appBar con Icon de Search

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -4,6 +4,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
         return Scaffold(
+            appBar: AppBar(
+                title: Text('Películas en cines'),
+                backgroundColor: Colors.indigoAccent,
+                actions: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () {},
+                    )
+                ],
+            ),
             body: SafeArea(
                 child: Text('Hola mundo'),
             )
