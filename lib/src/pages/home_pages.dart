a4cd206f0a985955be3b3b348bed9425814d9d24
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 13:29:57 -0500

    Se importo el Swiper y se utilizo en la funcion _swiperTarjetas

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+
+    Widget _swiperTarjetas() {
+        return Container(
+            padding: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 300.0,
+            child: Swiper(
+                layout: SwiperLayout.STACK,
+                itemWidth: 200.0,
+                itemBuilder: (BuildContext context,int index){
+                    return new Image.network(
+                        "http://via.placeholder.com/350x150",
+                        fit: BoxFit.fill,
+                    );
+                },
+                itemCount: 3,
+                // pagination: new SwiperPagination(),
+                // control: new SwiperControl(),
+            ),
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
                 title: Text('Películas en cines'),
@@ -14,9 +38,13 @@ class HomePage extends StatelessWidget {
                     )
                 ],
             ),
-            body: SafeArea(
-                child: Text('Hola mundo'),
+            body: Container(
+                child: Column(
+                    children: <Widget>[
+                        _swiperTarjetas()
+                    ],
+                ),
             )
         );
-  }
+    }
 }
