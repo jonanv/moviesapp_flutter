776a8cb8506c0da1b7e367a6587597c6e03296dd
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 12:46:36 -0500

    Se crearon las carpetas de src y pages y la clase home_page.dart, tambien se elimino la etiqueta debug en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
+//My imports
+import 'package:moviesapp_flutter/src/pages/home_pages.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
+            debugShowCheckedModeBanner: false,
             title: 'Peliculas',
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Material App Bar'),
-                ),
-                body: Center(
-                    child: Container(
-                        child: Text('Hello World'),
-                    ),
-                ),
-            ),
+            initialRoute: '/',
+            routes: {
+                '/' : (BuildContext context) => HomePage(),
+            },
         );
     }
 }
