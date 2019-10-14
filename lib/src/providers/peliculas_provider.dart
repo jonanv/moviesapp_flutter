bd0e757a674802ae7226e9b8443f81432dc2e48d
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-14 15:50:04 -0500

    Solucion de error en atributos de la clase provider dar_provider y se agrego funcion getEnCines()

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,14 @@
+import 'package:moviesapp_flutter/src/models/pelicula_model.dart';
+
 class PeliculasProvider {
-    String _apikey    = '',
-    String _url       = '',
-    String _language  = 'es-ES',
-    
+    String _apikey    = 'eeba7a481f04ca04446e13ca95af978d';
+    String _url       = 'api.themoviedb.org';
+    String _language  = 'es-ES';
+
+    Future<List<Pelicula>> getEnCines() async {
+        final url = Uri.https(_url, '3/movie/now_playing', {
+            'api_key'   : _apikey,
+            'language'  : _language
+        });
+    }
 }
